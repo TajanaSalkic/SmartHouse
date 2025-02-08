f86bebb75227c2b179a2d34df755884f763d21f3
Tajana Salkic <tajana.salkic.22@size.ba>
2025-02-09 00:12:38 +0100

    Enumi, klase (uređaj, rerna), interfejs IUredjaj

---
+++ SmartHouse/Enumi/VrstaKonekcije.cs
@@ -0,0 +1,5 @@
+﻿public enum VrstaKonekcije
+{
+    Bluetooth,
+    WiFi
+}
--- SmartHouse/Klase/Uredjaj.cs
+++ SmartHouse/Klase/Uredjaj.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartHouse.Interfejsi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace SmartHouse.Klase
 {
-    internal class Uredjaj
+    public class Uredjaj
     {
+        private String Naziv { get; set; }
+        private String MjestoUKuci { get; set; }
+        private Int64 Kolicina { get; set; }
+
+        private bool ukljucen {  get; set; }
+        private VrstaKonekcije VrstaKonekcije { get; set; }
+
+        public void ukljuci()
+        {
+            ukljucen = true;
+            Console.WriteLine(this.Naziv + " je ukljucen/ukljucena!");
+        }
+
+        public void iskljuci()
+        {
+            ukljucen = false;
+            Console.WriteLine(this.Naziv + " je iskljucen/iskljucena!");
+        }
     }
 }
